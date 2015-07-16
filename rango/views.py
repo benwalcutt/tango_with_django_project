471c849b123e1df727a9e2276dc2d59c153b1e1d
benwalcutt <bewalcut@email.uark.edu>
2015-07-16 12:12:43 -0500

    Added rango app

---
+++ rango/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import HttpResponse
+
+# Create your views here.
+
+def index(request):
+	return HttpResponse("Rango says hey there world!<br><a href=/rango/about>about</a>")
+
+def about(request):
+	return HttpResponse("Rango says here is the about page<br><a href=/rango>Back to main</a>")
